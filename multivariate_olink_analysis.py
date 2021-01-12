4bf81cc6088c2be89bb70aa7b681fd11cdb72cf6
PrimalCerebrate <33455128+PrimalCerebrate@users.noreply.github.com>
2021-01-12 10:30:50 +0100

    Added python script
    Python script with all libraries imported.

---
+++ multivariate_olink_analysis.py
@@ -0,0 +1,19 @@
+# can type in the python console `help(name of function)` to get the documentation
+from pydoc import help
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from scipy import stats
+from IPython.display import display, HTML
+
+# figures inline in notebook
+%matplotlib inline
+
+np.set_printoptions(suppress=True)
+
+DISPLAY_MAX_ROWS = 20  # number of max rows to print for a DataFrame
+pd.set_option('display.max_rows', DISPLAY_MAX_ROWS)
