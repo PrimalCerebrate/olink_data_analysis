5d1e10c3e06a3e1bcbc43b4dce8b84698ea784a8
PrimalCerebrate <33455128+PrimalCerebrate@users.noreply.github.com>
2021-01-13 20:50:52 +0100

    Added the part of the script, which creates subplots for each protein for each dataset.

--- multivariate_olink_analysis.py
+++ multivariate_olink_analysis.py
@@ -3,6 +3,7 @@ from pydoc import help
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import seaborn as sns
 from sklearn.preprocessing import scale
 from sklearn.decomposition import PCA
@@ -11,7 +12,7 @@ from scipy import stats
 from IPython.display import display, HTML
 
 # figures inline in notebook
-%matplotlib inline
+# %matplotlib inline
 
 np.set_printoptions(suppress=True)
 
@@ -37,6 +38,9 @@ data = pd.read_csv(
 dataattributes = pd.read_csv(
     "C:/umea_immunology/experiments/corona\olink_data/formatted_data/olink_sample_names.csv")
 
+# save data columns (without Sample) for the plotting later
+proteins = data.drop("Sample", axis=1).columns.str.strip()
+
 print(data.head())
 print(dataattributes.head())
 
@@ -60,5 +64,58 @@ additionaldata = additionaldata.reset_index(drop=True)
 data = pd.concat([data, additionaldata.drop("Sample", axis=1)],
                  axis=1)
 
-# %% Write data to new csv file
+# %% Save complete data to new csv file
 data.to_csv("C:/umea_immunology/experiments/corona/olink_data/formatted_data/20201969_Forsell_NPX_edit_complete.csv")
+
+data = data.drop("Sample", axis=1)
+
+# %% Create basic plot
+
+plt.style.use('ggplot')
+
+# divide data in different data sets (will produce a plot for each)
+
+data.columns = data.columns.str.strip()  # get rid of whitespace in first column
+
+# setup pdf for saving plots
+plotpages = PdfPages(
+    "C:/umea_immunology/experiments/corona/olink_data/olinkanalysis/preliminary_olink_data.pdf")
+
+
+# divide up data by dataset (one figure per dataset)
+datasets_unique = data["Dataset"].unique()
+for oneset in datasets_unique:
+    data_oneset = data[data["Dataset"] == oneset]
+
+    groups = data_oneset.groupby("Progress")
+    patients = data_oneset.groupby("PatientID")
+
+    patient_unique = data_oneset["Progress"].unique()
+    color_values = sns.color_palette("Set2", len(patient_unique))
+    color_map = dict(zip(patient_unique, color_values))
+
+    # begin figure
+    fig, ax = plt.subplots(nrows=9, ncols=5, figsize=(
+        30, 40))  # for 45 proteins
+    fig.tight_layout(pad=3)
+    # plot one graph per protein
+    for i, axis in enumerate(ax.flat):
+
+        # setup for plot
+        for name, group in groups:
+            axis.scatter(group["Day"], group[proteins[i]], label=name,
+                         c=group["Progress"].map(color_map))
+
+        axis.set_xlabel("Day")
+        axis.set_ylabel("Normalized Protein Amount")
+        axis.set_title(proteins[i]+" amounts in "+oneset+" samples")
+        axis.legend(loc="upper right", title="Progress")
+
+        for name, patient in patients:
+            axis.plot(patient["Day"], patient[proteins[i]],
+                      c=patient["Progress"].map(color_map).iloc[0])
+
+    # save plot
+    plotpages.savefig(fig)
+
+plotpages.close()
